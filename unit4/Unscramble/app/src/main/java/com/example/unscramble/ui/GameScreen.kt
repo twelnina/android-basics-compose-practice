178518f22e8eb82d32b5f5636f77d964b3c24418
twelnina <katsu12tem.dev@gmail.com>
2026-07-04 17:27:23 +0900

    feat: handle guess submission and error feedback

--- unit4/Unscramble/app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ unit4/Unscramble/app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -82,8 +82,9 @@ fun GameScreen(
         GameLayout(
             currentScrambledWord = gameUiState.currentScrambledWord,
             userGuess = gameViewModel.userGuess,
+            isGuessWrong = gameUiState.isGuessedWordWrong,
             onUserGuessChanged = { gameViewModel.updateUserGuess(it) },
-            onKeyboardDone = {},
+            onKeyboardDone = { gameViewModel.checkUserGuess() },
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
@@ -99,7 +100,7 @@ fun GameScreen(
 
             Button(
                 modifier = Modifier.fillMaxWidth(),
-                onClick = { }
+                onClick = { gameViewModel.checkUserGuess() }
             ) {
                 Text(
                     text = stringResource(R.string.submit),
@@ -138,6 +139,7 @@ fun GameStatus(score: Int, modifier: Modifier = Modifier) {
 @Composable
 fun GameLayout(
     currentScrambledWord: String,
+    isGuessWrong: Boolean,
     userGuess: String,
     onUserGuessChanged: (String) -> Unit,
     onKeyboardDone: () -> Unit,
@@ -184,8 +186,14 @@ fun GameLayout(
                     disabledContainerColor = colorScheme.surface,
                 ),
                 onValueChange = onUserGuessChanged,
-                label = { Text(stringResource(R.string.enter_your_word)) },
-                isError = false,
+                label = {
+                    if (isGuessWrong) {
+                        Text(stringResource(R.string.wrong_guess))
+                    } else  {
+                        Text(stringResource(R.string.enter_your_word))
+                    }
+                },
+                isError = isGuessWrong,
                 keyboardOptions = KeyboardOptions.Default.copy(
                     imeAction = ImeAction.Done
                 ),
--- unit4/Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ unit4/Unscramble/app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -66,5 +66,7 @@ class GameViewModel : ViewModel() {
                 )
             }
         }
+
+        updateUserGuess("")
     }
 }
