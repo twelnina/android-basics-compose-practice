53d38bdd2472f576aba5d01c5c17413aa05272fc
twelnina <katsu12tem.dev@gmail.com>
2026-07-06 00:06:28 +0900

    test: verify score update on correct guess

---
+++ unit4/Unscramble/app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -0,0 +1,30 @@
+package com.example.unscramble.ui.test
+
+import com.example.unscramble.data.SCORE_INCREASE
+import com.example.unscramble.data.getUnscrambledWord
+import com.example.unscramble.ui.GameViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Test
+
+class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdateAndErrorFlagUnset() {
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+    }
+
+    companion object{
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
+}
