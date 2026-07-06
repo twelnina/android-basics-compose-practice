cac419ac5dd04b348c29f7438283779d0f4f1a6d
twelnina <katsu12tem.dev@gmail.com>
2026-07-06 11:39:46 +0900

    test: add incorrect guess error state coverage

--- unit4/Unscramble/app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
+++ unit4/Unscramble/app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -5,6 +5,7 @@ import com.example.unscramble.data.getUnscrambledWord
 import com.example.unscramble.ui.GameViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class GameViewModelTest {
@@ -26,5 +27,18 @@ class GameViewModelTest {
 
     companion object{
         private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
+
+    @Test
+    fun gameViewModel_IncorrectGuess_ErrorFlagSet() {
+        val incorrectPlayerWord = "and"
+
+        viewModel.updateUserGuess(incorrectPlayerWord)
+        viewModel.checkUserGuess()
+
+        val currentGameUiState = viewModel.uiState.value
+
+        assertEquals(0, currentGameUiState.score)
+        assertTrue(currentGameUiState.isGuessedWordWrong)
     }
 }
