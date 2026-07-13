245d043abd3f2e96b153050f4596067dbf6b5e98
twelnina <katsu12tem.dev@gmail.com>
2026-07-13 23:31:19 +0900

    test(order): scaffold UI tests for the cupcake order screen

---
+++ unit4/Cupcake/app/src/androidTest/java/com/example/cupcake/test/CupcakeOrderScreenTest.kt
@@ -0,0 +1,40 @@
+package com.example.cupcake.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.example.cupcake.R
+import com.example.cupcake.ui.SelectOptionScreen
+import org.junit.Rule
+import org.junit.Test
+
+class CupcakeOrderScreenTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    @Test
+    fun selectOptionScreen_verifyContent() {
+        val flavors = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+        val subtotal = "$100"
+
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal = subtotal, options = flavors)
+        }
+
+        flavors.forEach { flavor ->
+            composeTestRule.onNodeWithText(flavor).assertIsDisplayed()
+        }
+
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.subtotal_price,
+                subtotal
+            )
+        ).assertIsDisplayed()
+
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsNotEnabled()
+    }
+}
