4411f6eea8c96e68a31c29a2659417cd97003641
twelnina <katsu12tem.dev@gmail.com>
2026-07-15 19:05:23 +0900

    feat(app-bar): add screen titles and back navigation

--- unit4/LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
+++ unit4/LunchTray/app/src/main/java/com/example/lunchtray/LunchTrayScreen.kt
@@ -15,19 +15,28 @@
  */
 package com.example.lunchtray
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.lunchtray.datasource.DataSource
 import com.example.lunchtray.ui.AccompanimentMenuScreen
@@ -37,25 +46,36 @@ import com.example.lunchtray.ui.OrderViewModel
 import com.example.lunchtray.ui.SideDishMenuScreen
 import com.example.lunchtray.ui.StartOrderScreen
 
-// TODO: AppBar
 
-enum class LunchTrayScreen {
-    Start,
-    EntreeMenu,
-    SideDishMenu,
-    AccompanimentMenu,
-    Checkout
+enum class LunchTrayScreen(
+    @param:StringRes
+    @get:StringRes
+    val title: Int
+) {
+    Start(title = R.string.start_order),
+    EntreeMenu(title = R.string.choose_entree),
+    SideDishMenu(title = R.string.choose_side_dish),
+    AccompanimentMenu(title = R.string.choose_accompaniment),
+    Checkout(title = R.string.order_checkout)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun LunchTrayApp() {
-    val navController = rememberNavController()
-    val viewModel: OrderViewModel = viewModel()
-
+fun LunchTrayApp(
+    viewModel: OrderViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = LunchTrayScreen.valueOf(
+        backStackEntry?.destination?.route ?: LunchTrayScreen.Start.name
+    )
     Scaffold(
         topBar = {
-            // TODO: AppBar
+            LunchTrayAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
         }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
@@ -143,4 +163,31 @@ fun resetOrderAndNavigateToStart(
 ) {
     viewModel.resetOrder()
     navController.popBackStack(LunchTrayScreen.Start.name, false)
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LunchTrayAppBar(
+    currentScreen: LunchTrayScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(stringResource(currentScreen.title))
+        },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(
+                    onClick = navigateUp
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
 }
